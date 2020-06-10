d26351d8a340abdc3a7c697f0cf78ccb2ba988f2
twarbelow <taija.warbelow@google.com>
2020-06-10 09:35:18 -0700

    add total inventory method, was very challenging for me

--- lib/market.rb
+++ lib/market.rb
@@ -1,5 +1,5 @@
 class Market
-  attr_reader :name, :vendors
+  attr_reader :name, :vendors, :total_inventory
 
   def initialize(name)
     @name = name
@@ -16,5 +16,20 @@ class Market
 
   def vendors_that_sell(item)
     vendors.find_all {|vendor| vendor.check_stock(item) > 0}
+  end
+
+  def total_inventory
+    # hash of item object keys each with a value of a hash with quantity and vendors
+    total_inventory = Hash.new(0)
+    vendors.each do |vendor|
+      vendor.inventory.each do |item, quantity|
+        total_inventory[item] = {quantity: 0, vendors: []} if !total_inventory.has_key?(item)
+        total_inventory[item][:quantity] += quantity
+        total_inventory[item][:vendors] << vendor
+      end
+    end
+
+
+    total_inventory
   end
 end
