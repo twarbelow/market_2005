d2cc255aac8c9b3c0ef00d218616835b9e4da319
twarbelow <taija.warbelow@google.com>
2020-06-10 08:34:49 -0700

    add vendor names test

--- test/market_test.rb
+++ test/market_test.rb
@@ -7,14 +7,14 @@ require './lib/market'
 class MarketTest < MiniTest::Test
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
-    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
     @vendor2 = Vendor.new("Ba-Nom-a-Nom")
     @vendor3 = Vendor.new("Palisade Peach Shack")
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-    @pry(main)> vendor1.stock(item1, 35)
+    @vendor1.stock(@item1, 35)
     @vendor1.stock(@item2, 7)
     @vendor2.stock(@item4, 50)
     @vendor2.stock(@item3, 25)
@@ -27,5 +27,13 @@ class MarketTest < MiniTest::Test
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
     assert_equal ([@vendor1, @vendor2, @vendor3]), @market.vendors
+  end
+
+  def test_it_has_vendor_names
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    assert_equal (["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]),
+    @market.vendor_names
   end
 end
