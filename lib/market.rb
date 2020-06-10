059469fd9d3b576d49c3817965caa7680471a6da
twarbelow <taija.warbelow@google.com>
2020-06-10 08:40:31 -0700

    add vendors that sell item method

--- lib/market.rb
+++ lib/market.rb
@@ -13,4 +13,8 @@ class Market
   def vendor_names
     vendors.map {|vendor| vendor.name}
   end
+
+  def vendors_that_sell(item)
+    vendors.find_all {|vendor| vendor.check_stock(item) > 0}
+  end
 end
