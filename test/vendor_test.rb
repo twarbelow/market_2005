3537d7aee1f3a383ef09f9a2db292d45aa4b99be
twarbelow <taija.warbelow@google.com>
2020-06-10 08:10:10 -0700

    add name and inventory test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -2,3 +2,17 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/item'
 require './lib/vendor'
+
+class VendorTest < MiniTest::Test
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_has_name
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+  end
+
+  def test_it_has_inventory
+    assert_equal ({}), @vendor.inventory
+  end
+end
