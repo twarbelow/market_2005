0e10f25835ac0a330c5692233f2b81120cde1d53
twarbelow <taija.warbelow@google.com>
2020-06-10 08:06:23 -0700

    add name and price test

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,3 +1,18 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/item'
+
+class ItemTest < MiniTest::Test
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  def test_it_has_name
+    assert_equal "Tomato", @item2.name
+  end
+
+  def test_it_has_price
+    assert_equal 0.50, @item2.price
+  end
+end
