0b3cd6fa6dcc43509a14889c6868ba2ff39c83d8
twarbelow <taija.warbelow@google.com>
2020-06-10 08:28:41 -0700

    fix setup

--- test/market_test.rb
+++ test/market_test.rb
@@ -8,14 +8,21 @@ class MarketTest < MiniTest::Test
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @pry(main)> vendor1.stock(item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
     @vendor3.stock(@item1, 65)
   end
 
   def test_it_can_add_vendor
+    assert_equal ([]), @market.vendors
     @market.add_vendor(@vendor1)
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
